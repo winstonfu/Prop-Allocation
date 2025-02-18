5cae1fea46773794b60c55928a83869b08c9f811
Winston Fu <winston.fu@gmail.com>
2025-02-18 14:54:46 +0800

    Refactoring.

--- prop_allocation.py
+++ prop_allocation.py
@@ -25,7 +25,8 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 
-class AllocationAlgGreedy:
+
+class AllocationAlgBase:
     def __init__(
             self,
             num_agents: int,
@@ -40,39 +41,7 @@ class AllocationAlgGreedy:
         self.rng = np.random.default_rng(seed = rng_seed)
         self.use_prop_1 = use_prop_1
         self.alpha_tracker = []
-
-        
-
-        if self.num_agents == 2:
-            # Set up solution-space
-            self.N = N
-            self.x = np.linspace(0, 1, self.N)  # array of x-values from 0 to 1
-            self.y = np.linspace(0, 1, self.N)  # array of y-values from 0 to 1
-            # Create a 2D mesh grid, X and Y will each be shape (N, N).
-            self.X, self.Y = np.meshgrid(self.x, self.y)
-            self.Z = np.empty((self.N, self.N), dtype=float)
-
-            # Initialize plots.
-            self.fig, self.ax = plt.subplots(2, 1, figsize=(6,5), layout='constrained', gridspec_kw={'height_ratios':[2,1]})
-            self.colormesh = self.ax[0].pcolormesh(self.X, self.Y, self.Z, cmap='plasma', shading='auto')
-            self.ax[0].set_title('t = 0')
-            self.ax[0].set_xlabel('x')
-            self.ax[0].set_ylabel('y')
-            self.cbar = self.fig.colorbar(self.colormesh, ax=self.ax[0])
-
-            self.marker, = self.ax[0].plot(0, 0, marker='x', color='red', markersize=10, mew=2)
-            self.line, = self.ax[1].plot([0], [0], 'k-+')
-
-            self.alpha_txt = self.ax[1].text(1, 0.98, rf'$\alpha_\mathrm{{min}}={self.num_agents}$',
-                                horizontalalignment='right',
-                                verticalalignment='top',
-                                transform=self.ax[1].transAxes)
-            
-            self.ax[1].set_xlabel('$t$')
-            self.ax[1].set_ylabel('$\\alpha$')
-
-           
-            self.t_list = []
+        self.t = 0
 
 
     def allocate_good(
@@ -80,7 +49,7 @@ class AllocationAlgGreedy:
             new_good_values: list
             ) -> None:
         '''
-        Allocate the new good.
+        Allocate the new good using the greedy algorithm.
         '''
         if self.good_values_list is None:
             self.add_to_bundle(self.rng.choice(range(self.num_agents)), new_good_values)
@@ -164,24 +133,9 @@ class AllocationAlgGreedy:
         Return alpha for a good, without changing the system.
         '''
         self.allocate_good(test_good)
-        foo = self.get_prop1_alpha()[0]
+        foo = np.min(self.get_prop1_alpha())
         self.remove_last_good()
         return foo
-    
-
-    def find_worst_good(self) -> list:
-        '''
-        Returns the worst good to add for 2 agents.
-        '''
-        # worst_good_index = np.argmin(self.Z)
-        # if worst_good_index == 0:
-        #     return [0.5, 0]
-        # else:
-        #     return [self.x[worst_good_index % self.N], self.y[worst_good_index // self.N]]
-
-        coords = np.array((self.X.ravel(), self.Y.ravel())).T.reshape(self.N, self.N, self.num_agents)
-        worst_good = self.rng.choice(coords[self.Z == np.min(self.Z)].reshape(-1, self.num_agents))
-        return [worst_good[0], worst_good[1]]
 
 
     def remove_last_good(self) -> None:
@@ -190,6 +144,48 @@ class AllocationAlgGreedy:
         '''
         self.bundles = np.delete(self.bundles, -1, 1)
         self.good_values_list = np.delete(self.good_values_list, -1, 0)
+
+
+class AllocationAlgGreedyTwo(AllocationAlgBase):
+    def __init__(
+            self,
+            rng_seed: int | None = None,
+            use_prop_1: bool = False,
+            N: int = 50,
+            adv_strat: str = 'greedy'
+                 ):
+        super().__init__(num_agents=2, rng_seed=rng_seed, use_prop_1=use_prop_1, N=N)
+
+        self.adv_strat = adv_strat
+
+        # Set up solution-space
+        self.N = N
+        self.x = np.linspace(0, 1, self.N)  # array of x-values from 0 to 1
+        self.y = np.linspace(0, 1, self.N)  # array of y-values from 0 to 1
+        # Create a 2D mesh grid, X and Y will each be shape (N, N).
+        self.X, self.Y = np.meshgrid(self.x, self.y)
+        self.Z = np.empty((self.N, self.N), dtype=float)
+
+        # Initialize plots.
+        self.fig, self.ax = plt.subplots(2, 1, figsize=(6,5), layout='constrained', gridspec_kw={'height_ratios':[2,1]})
+        self.colormesh = self.ax[0].pcolormesh(self.X, self.Y, self.Z, cmap='plasma', shading='auto')
+        self.ax[0].set_title('t = 0')
+        self.ax[0].set_xlabel('x')
+        self.ax[0].set_ylabel('y')
+        self.cbar = self.fig.colorbar(self.colormesh, ax=self.ax[0])
+
+        self.marker, = self.ax[0].plot(0, 0, marker='x', color='red', markersize=10, mew=2)
+        self.line, = self.ax[1].plot([0], [0], 'k-+')
+
+        self.alpha_txt = self.ax[1].text(1, 0.98, rf'$\alpha_\mathrm{{min}}={self.num_agents}$',
+                            horizontalalignment='right',
+                            verticalalignment='top',
+                            transform=self.ax[1].transAxes)
+        
+        self.ax[1].set_xlabel('$t$')
+        self.ax[1].set_ylabel('$\\alpha$')
+
+        self.t_list = []
 
 
     def update_solution_space(self) -> None:
@@ -201,26 +197,32 @@ class AllocationAlgGreedy:
                 self.Z[i, j] = self.test_allocation([self.x[i], self.y[j]])
 
 
-    def find_next_good(self, t) -> list:
+    def find_next_good(
+            self,
+            ) -> list:
         '''
         Find next good for 2 agents.
         '''
-        if t % 10 < 3:
-            return [self.rng.uniform(), self.rng.uniform()]
-        else:
+        if self.adv_strat == 'mixer':
+            if self.t % 10 < 3:
+                return [self.rng.uniform(), self.rng.uniform()]
+            else:
+                return self.find_worst_good()
+        elif self.adv_strat == 'greedy':
             return self.find_worst_good()
+        elif self.adv_strat == 'random':
+            return [self.rng.uniform(), self.rng.uniform()]
 
 
     def update_fig(self, t: int):
         '''
         Update figure for animation for 2 agents.
         '''
-        if not self.num_agents == 2:
-            logger.error('update_fig only works for 2 agents.')
+        self.t = t
         self.update_solution_space()
-        next_good = self.find_next_good(t)
+        next_good = self.find_next_good()
         self.allocate_good(next_good)
-        self.alpha_tracker.append(self.get_prop1_alpha()[0])
+        self.alpha_tracker.append(np.min(self.get_prop1_alpha()))
         self.t_list.append(t)
 
         self.colormesh.set_array(self.Z.ravel())
@@ -239,10 +241,25 @@ class AllocationAlgGreedy:
         Initialize figure.
         '''
         return (self.colormesh, self.marker, self.line,)
+    
+
+    def find_worst_good(self) -> list:
+        '''
+        Returns the worst good to add for 2 agents.
+        '''
+        # worst_good_index = np.argmin(self.Z)
+        # if worst_good_index == 0:
+        #     return [0.5, 0]
+        # else:
+        #     return [self.x[worst_good_index % self.N], self.y[worst_good_index // self.N]]
+
+        coords = np.array((self.X.ravel(), self.Y.ravel())).T.reshape(self.N, self.N, self.num_agents)
+        worst_good = self.rng.choice(coords[self.Z == np.min(self.Z)].reshape(-1, self.num_agents))
+        return [worst_good[0], worst_good[1]]
 
 
 def main():
-    alloc = AllocationAlgGreedy(2, rng_seed = 123)
+    alloc = AllocationAlgGreedyTwo(rng_seed = 123)
 
     anim = FuncAnimation(
         alloc.fig,
@@ -253,7 +270,7 @@ def main():
         init_func=alloc.init_fig
     )
 
-    anim.save('allocation_mixer.mp4')
+    anim.save('allocation_3.mp4')
 
     logger.info(f'Alpha: {alloc.alpha_tracker}')
     logger.info(f'Bundles: {alloc.bundles}')
